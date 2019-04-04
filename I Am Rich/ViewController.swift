aab01a212eb9579321835a5aa4fa9c3a953f5a56
lqtrung95 <44612846+lequocgiom@users.noreply.github.com>
2019-04-04 22:56:02 +0700

    First commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Le Trung on 4/4/19.
+//  Copyright © 2019 Le Trung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
